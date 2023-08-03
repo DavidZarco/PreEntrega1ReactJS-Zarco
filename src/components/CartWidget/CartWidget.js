04a42f2b97b906488925b3a40aad684a55b86d76
DavidZarco <89663792+DavidZarco@users.noreply.github.com>
2023-08-03 20:36:06 -0300

    2 files modified

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const CartWidget = () => {
 return (
-<div>
+
   <img src={"https://static.vecteezy.com/system/resources/thumbnails/002/238/646/small/isolated-of-shopping-cart-icon-basket-symbol-eps10-free-vector.jpg"} alt="cart-witget"/>
-  0
   
-</div>
+  
+
 );
 
 
